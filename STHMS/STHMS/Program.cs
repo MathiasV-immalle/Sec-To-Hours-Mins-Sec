c6d4a68475cb0f2af00ca9a4ab84d3d5b6b5869f
Vansant Mathias <mathias.vansant@immalle.eu>
2017-05-30 13:21:22 +0200

    Branch1

--- STHMS/STHMS/Program.cs
+++ STHMS/STHMS/Program.cs
@@ -8,8 +8,26 @@ namespace STHMS
 {
     class Program
     {
+        static void SecsToHoursMinsSecs(int totalSeconds, out int hours, out int mins, out int secs)
+        {
+            // tijdelijke tussentijdse variabele
+            int rest = 0;
+
+            // algoritme:
+            hours = totalSeconds / (60 * 60);
+            rest = totalSeconds % (60 * 60);
+            mins = rest / 60;
+            secs = rest % 60;
+        }
         static void Main(string[] args)
         {
+            int h = 0;
+            int m = 0;
+            int s = 0;
+
+            SecsToHoursMinsSecs(61, out h, out m, out s);
+
+            Console.WriteLine("{0} hours {1} minutes and {2} seconds", h, m, s);
         }
     }
 }
